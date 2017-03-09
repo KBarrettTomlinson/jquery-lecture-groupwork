0fbb305c944c83cb7f1e8540f4e006dd096800ad
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-03-09 15:11:04 -0600

    Assignment up through Hungry

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 var timesClicked = 0;
+var hungryLevel = 100;
 
 $(document).ready(function(){
   console.log("I'm here with you");
@@ -8,12 +9,24 @@ $(document).ready(function(){
       console.log('Youve Clicked Me!');
       timesClicked += 1;
       console.log(timesClicked);
-      var divideByThree = timesClicked % 3;
-      if (divideByThree === 0){
-          $(".container").append("<p>SUSHI</p>");
+      var modByThree = timesClicked % 3;
+      if (modByThree === 0){
+          appendSushi();
       }
-      
     });
+  $(".container").on("click",".sushi",function(){
+    $(this).remove();
+    hungryLevel -= 5;
+    console.log(hungryLevel);
+  });
 
 
 });
+
+
+function appendSushi(){
+  // $(".container").append("<div class='sushi'>"+"<p>SUSHI</p>"+"</div>");
+  $(".container").append("<div class='sushi'></div>");
+  var element = $(".container").children().last();
+  element.append("<p>SUSHI</p>");
+}
