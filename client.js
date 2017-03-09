14ab5ac15c9ee5b91dd89a9d1e3dfe107a291788
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-03-09 14:49:32 -0600

    Assignmetn up through SUSHI

--- client.js
+++ client.js
@@ -1,3 +1,19 @@
+var timesClicked = 0;
+
 $(document).ready(function(){
   console.log("I'm here with you");
+
+  $(".container").append("<button>"+"I'm A Button!"+"</button>");
+  $(".container").on("click","button",function(){
+      console.log('Youve Clicked Me!');
+      timesClicked += 1;
+      console.log(timesClicked);
+      var divideByThree = timesClicked % 3;
+      if (divideByThree === 0){
+          $(".container").append("<p>SUSHI</p>");
+      }
+      
+    });
+
+
 });
